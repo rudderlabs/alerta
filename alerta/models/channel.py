c9ad311e5034442113814aceeda5972d770d5880
Sai Varun Reddy Daram <saivarunvishal@gmail.com>
2021-12-13 18:45:26 +0530

    Adds validators

--- alerta/models/channel.py
+++ alerta/models/channel.py
@@ -1,8 +1,13 @@
 from typing import Any, Dict, List, Optional, Tuple, Union
+
+import validators
+
 from alerta.app import db
 
 
 class CustomerChannel:
+    SUPPORTED_CHANNEL_TYPES = ["webhook", "email"]
+
     def __init__(self, name, channel_type, properties, customer_id, id=None):
         self.id = id
         self.name = name
@@ -11,6 +16,32 @@ class CustomerChannel:
         self.customer_id = customer_id
 
     def create(self):
+        if not isinstance(self.name, str) or not self.name.strip():
+            raise Exception("Channel name must be a valid text value")
+        if self.channel_type not in CustomerChannel.SUPPORTED_CHANNEL_TYPES:
+            raise Exception(f"Support channel types are {CustomerChannel.SUPPORTED_CHANNEL_TYPES}")
+        if not isinstance(self.properties, dict):
+            raise Exception("Channel properties are required to create channel")
+        if self.channel_type == "email":
+            emails = self.properties.get("emails", [])
+            if not isinstance(emails, list) or len(emails) == 0:
+                raise Exception("Emails list cannot be empty")
+            for index, email in enumerate(emails):
+                if not isinstance(email, str) or email.strip() == "":
+                    raise Exception(f"Email value at position {index + 1} is not valid")
+                try:
+                    validators.email(email)
+                except Exception as e:
+                    raise Exception(f"Email value at position {index + 1} is not valid")
+        elif self.channel_type == "webhook":
+            url = self.properties.get('url')
+            if not isinstance(url, str) or not url.strip() == "":
+                raise Exception("Webhook property 'url' is required and cannot be empty")
+            try:
+                validators.url(url)
+            except Exception as e:
+                raise Exception("Webhook property 'url' is required and cannot be empty")
+
         return CustomerChannel.from_db(db.create_channel(self))
 
     @classmethod
