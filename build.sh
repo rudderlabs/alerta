78a9ffb2541f675e15edd5c4d9e1cdd1301bd55a
Sai Varun Reddy Daram <saivarunvishal@gmail.com>
2021-12-14 15:38:44 +0530

    Miscs

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 rm -rf rudder-alerta-enrichment-plugin
 git clone -b alert-enrichment-from-workspace-source-destination-tags git@github.com:rudderlabs/rudder-alerta-enrichment-plugin.git
-docker build -t rudderlabs/alerta:notification-service-v2 .
+docker build --build-arg=COMMIT_ID_VALUE="$(git log --format="%H" -n 1)" -t rudderlabs/alerta:notification-service-v2 .
 rm -rf rudder-alerta-enrichment-plugin
