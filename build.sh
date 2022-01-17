c7bdea8adec3136bcc96f8a86af1aeaf9e0cdb6c
Sai Varun Reddy Daram <saivarunvishal@gmail.com>
2022-01-17 22:24:32 +0530

    Chore: Adds github actions for alerta

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 rm -rf rudder-alerta-enrichment-plugin
-git clone -b $1 git@github.com:rudderlabs/rudder-alerta-enrichment-plugin.git
+git clone -b "$(cat RUDDER_ENRICH_PLUGIN_BRANCH_NAME)" git@github.com:rudderlabs/rudder-alerta-enrichment-plugin.git
 docker build --no-cache --build-arg=COMMIT_ID_VALUE="$(git log --format="%H" -n 1)" -t rudderlabs/alerta:$1 .
 rm -rf rudder-alerta-enrichment-plugin
