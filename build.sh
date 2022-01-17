216f4c3b0ef78822dcb27d9ce6530b43123ea2c6
Sai Varun Reddy Daram <saivarunvishal@gmail.com>
2022-01-17 22:06:24 +0530

    Chore: Changes to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 rm -rf rudder-alerta-enrichment-plugin
-git clone -b alert-enrichment-from-workspace-source-destination-tags git@github.com:rudderlabs/rudder-alerta-enrichment-plugin.git
-docker build --build-arg=COMMIT_ID_VALUE="$(git log --format="%H" -n 1)" -t rudderlabs/alerta:notification-service-v2 .
+git clone -b $1 git@github.com:rudderlabs/rudder-alerta-enrichment-plugin.git
+docker build --no-cache --build-arg=COMMIT_ID_VALUE="$(git log --format="%H" -n 1)" -t rudderlabs/alerta:$1 .
 rm -rf rudder-alerta-enrichment-plugin
